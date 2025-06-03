f68fb025628c410349e6dca2f6e8ea06f7861c24
Thomas de Grivel <thoxdg@gmail.com>
2025-06-03 17:32:58 +0200

    work around frame corruption leading to double-free

--- libkc3/env_eval.c
+++ libkc3/env_eval.c
@@ -566,6 +566,7 @@ bool env_eval_call_resolve (s_env *env, s_call *call)
 bool env_eval_callable (s_env *env, s_callable *callable,
                         s_tag *dest)
 {
+  s_callable c = {0};
   s_callable *tmp = NULL;
   assert(env);
   assert(callable);
@@ -579,8 +580,11 @@ bool env_eval_callable (s_env *env, s_callable *callable,
       return false;
     goto ok;
   case CALLABLE_FN:
-    if (! (tmp = callable_new_copy(callable)))
+    c = *callable;
+    c.data.fn.frame = frame_new_copy(env->frame);
+    if (! (tmp = callable_new_copy(&c)))
       return false;
+    frame_delete(c.data.fn.frame);
     if (! tmp->data.fn.module)
       tmp->data.fn.module = env->current_defmodule;
     goto ok;
